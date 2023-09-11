e3d71268a62ad947b329590e8855df6bb21dc3e2
Rhababo <Wesley.Adams@rhababo.com>
2023-09-11 18:07:18 -0500

    debug to keep spider-man

--- javascripts/discourse/helpers/gif-modal.js
+++ javascripts/discourse/helpers/gif-modal.js
@@ -4,6 +4,9 @@ export function showGifModal() {
   console.log("showGifModal");
   console.log(this);
   console.log(this.attrs);
+  console.log(this.parentView.attrs.topic.value.tags);
+  console.log(settings.tags);
+  console.log(this.parentView.attrs.topic.value.tags.includes(settings.tags));
   showModal("gif", {
     title: themePrefix("gif.modal_title"),
   }).setProperties({
